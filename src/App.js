e5f849250ee6b0004aff6d33b499fe0e5a53f94a
“Victory <victory23tshimbadi@gmail.com>
2023-04-04 13:30:58 +0100

    add routeur

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Register from "./pages/Register"
 import Login from './pages/Login';
 
 function App() {
   return (
-    <Login/>
+    <BrowserRouter>
+			<Routes>
+				<Route path="/" index element={<Register />} />
+				<Route path="/admin" element={<Login />} />
+			</Routes>
+		</BrowserRouter>
   );
 }
 
